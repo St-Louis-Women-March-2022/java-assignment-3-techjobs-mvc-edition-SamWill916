49d8f5a42f967ce380c5f559125bbefcfaffc454
S W <sw@Ss-MacBook-Air.local>
2022-11-04 21:18:10 -0500

    Display Search Results

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -26,7 +26,7 @@ public class SearchController {
         model.addAttribute("columns", columnChoices);
         return "search";
     }
-
+    // TODO #3 - Create a handler to process a search request and render the updated search view.
     @PostMapping(value = "results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
@@ -41,7 +41,7 @@ public class SearchController {
         model.addAttribute("columns", columnChoices);
         return "search";
 
-        // TODO #3 - Create a handler to process a search request and render the updated search view.
+
 
     }
 }
